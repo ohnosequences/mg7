b83baf7a2f0c02b04bf2f08e4104ef95a9430299
Alexey Alekhin <laughedelic@gmail.com>
2015-08-30 16:22:00 +0200

    Updating to the current loquat

--- src/main/scala/flash/config.scala
+++ src/main/scala/flash/config.scala
@@ -1,13 +1,13 @@
 package ohnosequences.metagenomica.flash
 
-import ohnosequences.datasets._, s3Locations._
+import data._
 
-import ohnosequences.cosas.typeSets._
+import ohnosequences.datasets._, s3Locations._, illumina._, reads._
 
-import ohnosequences.loquat.utils._
-import ohnosequences.loquat.configs._
-import ohnosequences.loquat.dataMappings._
-import ohnosequences.loquat.instructions._
+import ohnosequences.cosas._, typeSets._, types._
+import ohnosequences.cosas.ops.typeSets._
+
+import ohnosequences.loquat._, utils._, configs._, dataMappings._, dataProcessing._
 
 import ohnosequences.statika.bundles._
 import ohnosequences.statika.aws._, api._, amazonLinuxAMIs._
@@ -15,68 +15,64 @@ import ohnosequences.statika.aws._, api._, amazonLinuxAMIs._
 import ohnosequences.awstools._, regions.Region._, s3.ObjectAddress, ec2.InstanceType._
 import ohnosequences.awstools.autoscaling._
 
-import java.io.File
-import era7.project._, loquats._
-
-
-trait flashConfigTest extends Era7LoquatConfig {
-
-  type FlashInstructions <: AnyFlashInstructions
-  val  flashInstructions: FlashInstructions
-
-  //val metadata: AnyArtifactMetadata = generated.metadata.CobetiaRna
-
-  val managerConfig = ManagerConfig(
-    instanceType = m3_medium,
-    purchaseModel = SpotAuto
-  )
-
-  val workersConfig = WorkersConfig(
-    instanceType = m3_medium,
-    purchaseModel = SpotAuto,
-    groupSize = WorkersGroupSize(0, 1, 10)
-  )
-
-  val terminationConfig = TerminationConfig(
-    terminateAfterInitialDataMappings = true
-  )
-
-  val dataMappings: List[AnyDataMapping] = List(
-      DataMapping(
-        id = "ERR567374_1",
-        instructions = flashInstructions
-      )(remoteInput =
-          flashInstructions.reads1.atS3(ObjectAddress("resources.ohnosequences.com", "16s/public-datasets/PRJEB6592/reads/ERR567374_1.fastq.gz")) :~:
-          flashInstructions.reads2.atS3("resources.ohnosequences.com", "16s/public-datasets/PRJEB6592/reads/ERR567374_2.fastq.gz") :~:
-          ∅,
-        remoteOutput =
-          flashInstructions.merged.atS3("resources.ohnosequences.com", "16s/public-datasets/PRJEB6592/flash-test/ERR567374_1.merged.fastq") :~:
-          flashInstructions.stats.atS3("resources.ohnosequences.com", "16s/public-datasets/PRJEB6592/flash-test/ERR567374_1.stats.txt") :~:
-          ∅,
-      )
-    )
-
-}
+// import java.io.File
+import era7.project.loquats._
 
 
 case object flashTest {
-  case object testInstructions extends AnyFlashInstructions {
+  val readsType = illumina.PairedEnd(bp300, InsertSize(3000))
+  case object reads1 extends PairedEnd1Fastq(readsType, "ERR567374_1.fastq.gz")
+  case object reads2 extends PairedEnd2Fastq(readsType, "ERR567374_2.fastq.gz")
 
-    type ReadsType = PairedEnd[InsertSize, bp300.type]
-    val readsType = illumina.PairedEnd(bp300, InsertSize(3000))
+  case object testData extends FlashData(readsType, reads1, reads2)
 
-    type Reads1 = ???
-    val reads1 = ???
+  case object testInstructions extends FlashInstructions(testData)
 
-    type Reads2 = ???
-    val reads2 = ???
+  case object testConfig extends Era7LoquatConfig {
+
+    // type FlashInstructions <: AnyFlashInstructions
+    // val  flashInstructions: FlashInstructions
+
+    // val metadata: AnyArtifactMetadata = generated.metadata.metagenomica
+
+    val managerConfig = ManagerConfig(
+      instanceType = m3_medium,
+      purchaseModel = SpotAuto
+    )
+
+    val workersConfig = WorkersConfig(
+      instanceType = m3_medium,
+      purchaseModel = SpotAuto,
+      groupSize = WorkersGroupSize(0, 1, 10)
+    )
+
+    val terminationConfig = TerminationConfig(
+      terminateAfterInitialDataMappings = true
+    )
+
+    val dataMappings: List[AnyDataMapping] = List(
+        DataMapping(
+          id = "ERR567374_1",
+          dataProcessing = testInstructions
+        )(remoteInput =
+            testInstructions.data.reads1.atS3(ObjectAddress("resources.ohnosequences.com", "16s/public-datasets/PRJEB6592/reads/ERR567374_1.fastq.gz")) :~:
+            testInstructions.data.reads2.atS3(ObjectAddress("resources.ohnosequences.com", "16s/public-datasets/PRJEB6592/reads/ERR567374_2.fastq.gz")) :~:
+            ∅,
+          remoteOutput =
+            testInstructions.data.merged.atS3(ObjectAddress("resources.ohnosequences.com", "16s/public-datasets/PRJEB6592/flash-test/ERR567374_1.merged.fastq")) :~:
+            testInstructions.data.stats.atS3(ObjectAddress("resources.ohnosequences.com", "16s/public-datasets/PRJEB6592/flash-test/ERR567374_1.stats.txt")) :~:
+            ∅
+        )
+      )
+
   }
 
-  case object testConfig extends flashConfigTest {
 
-    type FlashInstructions = testInstructions.type
-    val  flashInstructions = testInstructions
-  }
+  // case object testConfig extends flashConfigTest {
+  //
+  //   type FlashInstructions = testInstructions.type
+  //   val  flashInstructions = testInstructions
+  // }
 
   case object testLoquat extends Loquat(testConfig, testInstructions)
 }
--- src/main/scala/flash/instructions.scala
+++ src/main/scala/flash/instructions.scala
@@ -1,57 +1,42 @@
 package ohnosequences.metagenomica.flash
 
-import ohnosequences.loquat.instructions._
-import ohnosequences.statika.instructions._
+import ohnosequences.loquat._, utils._, dataProcessing._
+import ohnosequences.statika._, bundles._, instructions._
 import ohnosequences.flash._, api._, data._
 import ohnosequences.cosas._, typeSets._, types._
 import ohnosequences.datasets._, dataSets._, fileLocations._, illumina._, reads._
 import ohnosequencesBundles.statika.Flash
 import java.io.File
 
-case object flash extends Flash("1.2.11")
+case object flashBundle extends Flash("1.2.11")
 
-trait AnyFlashInstructions extends AnyInstructionsBundle {
+trait AnyFlashInstructions extends AnyDataProcessingBundle {
 
-  val bundleDependecies: List[AnyBundle] = List(flash)
+  type Data <: AnyFlashData
+  val  data: Data
 
-  // Paired end reads as input
-  type ReadsType <: AnyReadsType { type EndType = pairedEndType }
-  val readsType: ReadsType
+  type Input = Data#Input
+  val  input = data.input
 
-  type Reads1 <: AnyPairedEnd1Fastq { type DataType = ReadsType }
-  val reads1: Reads1
+  type Output = Data#Output
+  val  output = data.output
 
-  type Reads2 <: AnyPairedEnd2Fastq { type DataType = ReadsType }
-  val reads2: Reads2
+
+  def instructions: AnyInstructions = say("I'm fine")
+
+  val bundleDependencies: List[AnyBundle] = List(flashBundle)
 
   // TODO FLASh stuff change options, derived from reads type
-  lazy val flashOptions = flash.defaults update (
-    read_len(readsType.length.length)   :~:
-    maxOverlap(readsType.length.length) :~: ∅
-  )
-
-  type Merged = MergedReads[ReadsType, Reads1, Reads2]
-  lazy val merged: Merged = new MergedReads(readsType, reads1, reads2, flashOptions)
-
-  type Stats = MergedReadsStats[Merged]
-  val  stats: Stats = new MergedReadsStats(merged)
-
-  type     Input = Reads1 :^: Reads2 :^: DNil
-  lazy val input = reads1 :^: reads2 :^: DNil
-
-  type     Output = Merged :^: Stats :^: DNil
-  lazy val output = merged :^: stats :^: DNil
-
   final def processData(
     dataMappingId: String,
-    inputFiles: InputFiles
+    context: Context
   ): AnyInstructions.withOut[OutputFiles] = {
 
-    val reads1gz = getFile(inputFiles, reads1).getCanonicalPath
-    val reads2gz = getFile(inputFiles, reads2).getCanonicalPath
+    val reads1gz: file = context.file(data.reads1)
+    val reads2gz: file = context.file(data.reads2)
 
-    val reads1fastq = reads1gz.stripSuffix(".gz")
-    val reads2fastq = reads2gz.stripSuffix(".gz")
+    val reads1fastq: file = reads1gz.rename( _.stripSuffix(".gz") )
+    val reads2fastq: file = reads2gz.rename( _.stripSuffix(".gz") )
 
     // define input
     lazy val flashInput = FlashInputAt(
@@ -68,7 +53,7 @@ trait AnyFlashInstructions extends AnyInstructionsBundle {
         api.input(flashInput)     :~:
         api.output(flashOutput)   :~: ∅
       ),
-      flashOptions
+      data.flashOptions
     )
 
     // run expression, hope for the best
@@ -77,8 +62,13 @@ trait AnyFlashInstructions extends AnyInstructionsBundle {
     flashExpr.cmd -&-
     success(
       s"FLASh merged reads from ${dataMappingId}, much success so fast",
-      (merged inFile flashOutput.mergedReads)             :~:
-      (stats  inFile flashOutput.lengthNumericHistogram)  :~: ∅
+      data.merged.inFile(flashOutput.mergedReads)           :~:
+      data.stats.inFile(flashOutput.lengthNumericHistogram) :~: ∅
     )
   }
+}
+
+class FlashInstructions[D <: AnyFlashData](val flashData: D) extends AnyFlashInstructions {
+
+  type Data = D
 }
