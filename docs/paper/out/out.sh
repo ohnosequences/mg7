7cc3e184b0adb689752829fcdd01657869c334c5
Eduardo Pareja-Tobes <eparejatobes@ohnosequences.com>
2015-09-12 19:30:45 +0200

    generate pdf, refs working -- paper

--- docs/paper/out/out.sh
+++ docs/paper/out/out.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 set -x
 ME=`dirname $0`
-pandoc --template=${ME}/frontiers.tex ${ME}/../paper.md --smart -s -o ${ME}/paper.tex
+pandoc --natbib --template=${ME}/frontiers.tex ${ME}/../paper.md --smart -s -o ${ME}/paper.tex
 cd ${ME}
 pdflatex -halt-on-error paper.tex
+bibtex paper
 pdflatex -halt-on-error paper.tex
