c321fc7e676c64dc7ae31efaeb59805d41dbb389
Alexey Alekhin <laughedelic@gmail.com>
2015-12-15 18:42:38 +0100

    Updated dependencies to non-snapshot versions (still a lot of milestones); resolves well, but needs fixes in the code

--- build.sbt
+++ build.sbt
@@ -15,32 +15,29 @@ resolvers := Seq(
 
 libraryDependencies ++= Seq(
   // APIs:
-  "ohnosequences" %% "flash"       % "0.2.0-M1",
-  "ohnosequences" %% "blast"       % "0.2.0-M1",
-  "ohnosequences" %% "fastarious"  % "0.1.0",
+  "ohnosequences" %% "flash"       % "0.2.0",
+  "ohnosequences" %% "blast"       % "0.2.0",
+  "ohnosequences" %% "fastarious"  % "0.2.0",
   // generic tools:
-  "ohnosequences" %% "cosas"       % "0.7.1",
-  "ohnosequences" %% "loquat"      % "2.0.0-futures-SNAPSHOT",
-  "ohnosequences" %% "datasets"    % "0.2.0-M2",
+  "ohnosequences" %% "cosas"       % "0.8.0",
+  "ohnosequences" %% "loquat"      % "2.0.0-M5",
+  "ohnosequences" %% "datasets"    % "0.2.0",
   "ohnosequences" %% "statika"     % "2.0.0-M4",
-  "ohnosequences" %% "aws-statika" % "2.0.0-M5",
+  "ohnosequences" %% "aws-statika" % "2.0.0-M7",
   // bundles:
   "ohnosequences-bundles" %% "flash"      % "0.1.0",
   "ohnosequences-bundles" %% "blast"      % "0.2.0",
-  "ohnosequences-bundles" %% "bio4j-dist" % "0.1.0-M1",
+  "ohnosequences-bundles" %% "bio4j-dist" % "0.1.0-M2",
   // utils:
-  "era7" %% "project-utils" % "0.1.0-loquat-testing-SNAPSHOT",
+  "era7" %% "project-utils" % "0.1.0-M5",
   // testing:
   "org.scalatest" %% "scalatest" % "2.2.5" % Test
 )
 
 dependencyOverrides ++= Set(
-  "com.fasterxml.jackson.core" % "jackson-core"        % "2.3.2",
-  "com.fasterxml.jackson.core" % "jackson-databind"    % "2.3.2",
-  "com.fasterxml.jackson.core" % "jackson-annotations" % "2.3.2",
   "commons-logging"            % "commons-logging"     % "1.1.3",
   "commons-codec"              % "commons-codec"       % "1.7",
-  "org.apache.httpcomponents"  % "httpclient"          % "4.5",
+  "org.apache.httpcomponents"  % "httpclient"          % "4.5.1",
   "org.slf4j"                  % "slf4j-api"           % "1.7.7"
 )
 
--- src/main/scala/metagenomica/test.scala
+++ src/main/scala/metagenomica/test.scala
@@ -68,7 +68,8 @@ case object test {
   }
 
   abstract class TestLoquatConfig[DP <: AnyDataProcessingBundle]
-    (val dataProcessing: DP) extends AnyTestLoquatConfig {
+    (val loquatName: String, val dataProcessing: DP) extends AnyTestLoquatConfig {
+
     type DataProcessing = DP
   }
 
@@ -85,7 +86,7 @@ case object testLoquats {
   val commonS3Prefix = S3Folder("resources.ohnosequences.com", "16s/public-datasets/PRJEB6592")
 
 
-  case object flashConfig extends TestLoquatConfig(flashDataProcessing(testParameters)) {
+  case object flashConfig extends TestLoquatConfig("flash", flashDataProcessing(testParameters)) {
 
     val dataMappings: List[DataMapping[DataProcessing]] = sampleIds map { sampleId =>
       DataMapping(sampleId, dataProcessing)(
@@ -105,7 +106,7 @@ case object testLoquats {
 
 
 
-  case object splitConfig extends TestLoquatConfig(splitDataProcessing) {
+  case object splitConfig extends TestLoquatConfig("split", splitDataProcessing) {
 
     val dataMappings: List[DataMapping[DataProcessing]] = flashConfig.dataMappings.map { flashData =>
       DataMapping(flashData.id, dataProcessing)(
@@ -119,7 +120,7 @@ case object testLoquats {
   case object splitLoquat extends TestLoquat(splitConfig)
 
 
-  case object blastConfig extends TestLoquatConfig(blastDataProcessing(testParameters)) {
+  case object blastConfig extends TestLoquatConfig("blast", blastDataProcessing(testParameters)) {
 
     lazy val dataMappings: List[DataMapping[DataProcessing]] =
       splitConfig.dataMappings flatMap { splitData: DataMapping[splitDataProcessing.type] =>
@@ -143,7 +144,7 @@ case object testLoquats {
 
 
 
-  case object mergeConfig extends TestLoquatConfig(mergeDataProcessing) {
+  case object mergeConfig extends TestLoquatConfig("merge", mergeDataProcessing) {
 
     lazy val dataMappings: List[DataMapping[DataProcessing]] =
       splitConfig.dataMappings map { splitData =>
@@ -163,7 +164,7 @@ case object testLoquats {
   case object mergeLoquat extends TestLoquat(mergeConfig)
 
 
-  case object assignmentConfig extends TestLoquatConfig(assignmentDataProcessing(testParameters)) {
+  case object assignmentConfig extends TestLoquatConfig("assignment", assignmentDataProcessing(testParameters)) {
 
     val dataMappings: List[DataMapping[DataProcessing]] = sampleIds map { sampleId =>
       DataMapping(sampleId, dataProcessing)(
@@ -182,7 +183,7 @@ case object testLoquats {
   case object assignmentLoquat extends TestLoquat(assignmentConfig)
 
 
-  case object countingConfig extends TestLoquatConfig(countingDataProcessing) {
+  case object countingConfig extends TestLoquatConfig("counting", countingDataProcessing) {
 
     val dataMappings: List[DataMapping[DataProcessing]] = assignmentConfig.dataMappings.map { assignmentData =>
       DataMapping(assignmentData.id, dataProcessing)(
