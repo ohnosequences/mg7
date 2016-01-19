8eba64f11d61fba23d7903ea6a6c26839311a733
Alexey Alekhin <laughedelic@gmail.com>
2016-01-19 17:18:24 +0100

    WIP: fixing code; restored split loquat code; changed files-related code

--- src/main/scala/metagenomica/data.scala
+++ src/main/scala/metagenomica/data.scala
@@ -5,24 +5,23 @@ import ohnosequences.cosas._, types._, klists._
 
 case object data {
 
-  // many files in a virtual S3 folder:
-  // case object ChunksDataType extends AnyDataType
-
-
-  // Flash input:
+  // Flash:
   case object pairedReads1 extends FileData("reads1")("fastq.gz")
   case object pairedReads2 extends FileData("reads2")("fastq.gz")
 
-  case object flashInput extends DataSet(pairedReads1 :×: pairedReads2 :×: |[AnyData])
-
-  // Flash output:
   case object mergedReads extends FileData("reads")("fastq")
   case object flashStats extends FileData("stats")("txt")
 
+  case object flashInput extends DataSet(pairedReads1 :×: pairedReads2 :×: |[AnyData])
   case object flashOutput extends DataSet(mergedReads :×: flashStats :×: |[AnyData])
 
-  // Reads after splitting:
+
+  // Reads after splitting (multiple files in a virtual S3 folder):
   case object readsChunks extends Data("reads-chunks")
+
+  case object splitInput extends DataSet(mergedReads :×: |[AnyData])
+  case object splitOutput extends DataSet(readsChunks :×: |[AnyData])
+
 
   // Blast input:
   case object readsChunk extends FileData("reads")("fastq")
--- src/main/scala/metagenomica/loquats/2.split.scala
+++ src/main/scala/metagenomica/loquats/2.split.scala
@@ -1,59 +1,54 @@
-// package ohnosequences.metagenomica.loquats
-//
-// import ohnosequences.metagenomica._
-//
-// import ohnosequences.loquat._
-//
-// import ohnosequences.statika._
-//
-// import ohnosequences.cosas._, typeSets._, properties._, records._
-//
-// import ohnosequences.datasets._
-//
-// import better.files._
-// import java.nio.file._
-// import collection.JavaConversions._
-//
-//
-// case object splitDataProcessing extends DataProcessingBundle()(
-//   input = data.mergedReads :^: DNil,
-//   output = data.readsChunks :^: DNil
-// ) {
-//
-//   def instructions: AnyInstructions = say("Splitting, cutting, separating")
-//
-//   def processData(
-//     dataMappingId: String,
-//     context: Context
-//   ): Instructions[OutputFiles] = {
-//
-//     val outputDir = context / "chunks"
-//
-//     // TODO: move it to the config
-//     val chunkSize = 1
-//
-//     LazyTry {
-//       outputDir.createDirectories()
-//
-//       lazy val chunks: Iterator[(Seq[String], Int)] =
-//         io.Source.fromFile( context.file(data.mergedReads).toJava )
-//           .getLines
-//           .grouped(4 * chunkSize)
-//           .zipWithIndex
-//
-//       chunks foreach { case (chunk, n) =>
-//         Files.write(
-//           (outputDir / s"chunk.${n}.fastq").path,
-//           asJavaIterable(chunk),
-//           StandardOpenOption.CREATE,
-//           StandardOpenOption.WRITE
-//         )
-//       }
-//     } -&-
-//     success(
-//       "much blast, very success!",
-//       data.readsChunks.inFile(outputDir) :~: ∅
-//     )
-//
-//   }
-// }
+package ohnosequences.metagenomica.loquats
+
+import ohnosequences.metagenomica._
+
+import ohnosequences.loquat._
+
+import ohnosequences.statika._
+
+import ohnosequences.cosas._, types._, klists._
+
+import ohnosequences.datasets._
+
+import better.files._
+import java.nio.file._
+import collection.JavaConversions._
+
+
+case object splitDataProcessing extends DataProcessingBundle()(
+  input = data.splitInput,
+  output = data.splitOutput
+) {
+
+  def instructions: AnyInstructions = say("Splitting, cutting, separating")
+
+  def process(context: ProcessingContext[Input]): AnyInstructions { type Out <: OutputFiles } = {
+
+    val outputDir = context / "chunks"
+
+    // TODO: move it to the config
+    val chunkSize = 1
+
+    LazyTry {
+      outputDir.createDirectories()
+
+      lazy val chunks: Iterator[(Seq[String], Int)] =
+        context.inputFile(data.mergedReads)
+          .lines
+          .grouped(4 * chunkSize)
+          .zipWithIndex
+
+      chunks foreach { case (chunk, n) =>
+        (outputDir / s"chunk.${n}.fastq")
+          .createIfNotExists()
+          .overwrite(chunk.mkString("\n"))
+      }
+    } -&-
+    success(
+      "much blast, very success!",
+      data.readsChunks(outputDir) ::
+      *[AnyDenotation { type Value <: FileResource }]
+    )
+
+  }
+}
