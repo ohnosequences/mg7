6a4c1f105ccb947faf1073ec9fb3b1499efe57a3
Eduardo Pareja-Tobes <eparejatobes@ohnosequences.com>
2016-07-23 18:03:52 +0200

    review summary loquat

--- src/main/scala/mg7/loquats/8.summary.scala
+++ src/main/scala/mg7/loquats/8.summary.scala
@@ -1,28 +1,23 @@
 package ohnosequences.mg7.loquats
 
-import ohnosequences.mg7._
-
-import ohnosequences.mg7.bio4j._, taxonomyTree._, titanTaxonomyTree._
-
+import ohnosequences.mg7._, bio4j._, taxonomyTree._, titanTaxonomyTree._
 import ohnosequences.loquat._
-
 import ohnosequences.statika._
 import ohnosequences.cosas._, types._, klists._
 import ohnosequences.datasets._
 import ohnosequences.fastarious._, fasta._
-
 import better.files._
 import com.github.tototoshi.csv._
-
 import com.bio4j.titan.model.ncbiTaxonomy.TitanNCBITaxonomyGraph
 
-
 case object summaryDataProcessing extends DataProcessingBundle()(
-  input = data.summaryInput,
-  output = data.summaryOutput
-) {
+  input   = data.summaryInput,
+  output  = data.summaryOutput
+)
+{
 
   def countReads(file: File): Integer = {
+    // TODO why io.Source??
     val source = io.Source.fromFile( file.toJava )
     val readsNumber = fasta.parseMap( source.getLines ).length
     source.close()
@@ -30,7 +25,6 @@ case object summaryDataProcessing extends DataProcessingBundle()(
   }
 
   def countLines(file: File): Integer = { file.lines.length }
-
 
   def instructions: ohnosequences.statika.AnyInstructions = say("Running summary loquat")
 
