518034722ba3bd0695ad4f45893a8cbce7c0dc75
Eduardo Pareja-Tobes <eparejatobes@ohnosequences.com>
2016-07-23 18:12:14 +0200

    review bio4j bundle

--- src/main/scala/mg7/bio4j/bundle.scala
+++ src/main/scala/mg7/bio4j/bundle.scala
@@ -3,12 +3,10 @@ package ohnosequences.mg7.bio4j
 import ohnosequences.statika._
 import ohnosequences.awstools.s3._
 import ohnosequencesBundles.statika._
-
 import com.thinkaurelius.titan.core._
 import com.bio4j.titan.model.ncbiTaxonomy._
 import com.bio4j.titan.util.DefaultTitanGraph
 import org.apache.commons.configuration.Configuration
-
 
 case object taxonomyBundle extends AnyBio4jDist {
 
@@ -23,18 +21,3 @@ case object taxonomyBundle extends AnyBio4jDist {
       new DefaultTitanGraph(TitanFactory.open(configuration))
     )
 }
-
-
-// case object bio4jBundleTest {
-//
-//   import ohnosequences.statika.aws._
-//   import ohnosequences.awstools.ec2._
-//   import ohnosequences.awstools.regions.Region._
-//
-//   case object bio4jTaxonomyCompat extends Compatible(
-//     amznAMIEnv(AmazonLinuxAMI(Ireland, HVM, InstanceStore)),
-//     bio4jNCBITaxonomy,
-//     generated.metadata.mg7
-//   )
-//
-// }
